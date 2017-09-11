8ed6fc4bfdae6c913aa42c114917f0f444102997
f1488 <4188>
2017-09-11 11:48:26 -0700

    Removed files

--- setup_tracker_server.js
+++
@@ -1,5 +0,0 @@
-
-var Server = require('bittorrent-tracker').Server
-var server = new Server({udp : true, http : false, ws : false, stats : false})
-server.on('start', function(addr) {console.log('got start message from ' + addr)})
-server.listen(3000)
