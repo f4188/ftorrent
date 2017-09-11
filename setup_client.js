6730c10b7c0f1efcb9100064d8b362756a255711
f1488 <4188>
2017-09-11 11:47:32 -0700

    removed files

--- setup_client.js
+++
@@ -1,7 +0,0 @@
-
-
-Downloader = require('./swarm/index.js').Downloader
-
-var client = new Downloader()
-client.setupWithMetaInfoFile('.../c_primer_5th_edition.pdf')
-client.start().catch(x => console.log(x))
